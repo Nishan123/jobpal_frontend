4109037c06aa2a603f215043330f2c42661804c0
Nishan <girinishan1234567890@gmail.com>
2025-02-10 19:15:23 +0545

    some responsive changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './components/Home';
 import Jobs from './components/Jobs';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PostJob from './components/PostJob';
 import SaveJobs from './components/SaveJobs';
 import Discussion from './components/Discussion';
@@ -13,16 +13,14 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/jobs" element={<Jobs />} />
-            <Route path="/post-job" element={<PostJob />} />
-            <Route path="/apply-jobs" element={<ApplyJobs />} />
-            <Route path="/saved-job" element={<SaveJobs />} />
-            <Route path="/discussion" element={<Discussion />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/post-job" element={<PostJob />} />
+          <Route path="/apply-jobs" element={<ApplyJobs />} />
+          <Route path="/saved-job" element={<SaveJobs />} />
+          <Route path="/discussion" element={<Discussion />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
